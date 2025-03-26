9d82e81ca5e31d9c7cfe080fb915fa5b4dcc249a
Ronierison <rmoreira@descartes.com>
2025-03-25 21:57:59 -0300

    insert the rest assured dependency and creation of list metadata test

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.+"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.rest-assured:rest-assured:5.5.1")
 }
 
 tasks.test {
--- src/test/java/automation/api/AppTest.java
+++ src/test/java/automation/api/AppTest.java
@@ -1,12 +1,22 @@
 package automation.api;
 
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
     @Test
-    public void testAppHasAGreeting() {
-        assertEquals(1, 1);
+    public void testUserListData() {
+        when().
+                get(("https://reqres.in/api/users?page=2")).
+        then().
+                statusCode(HttpStatus.SC_OK).
+                body("page", is(2)).
+                body("data", is(notNullValue()));
     }
 }
 
