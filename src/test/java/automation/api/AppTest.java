7bf9d92a91b7793f4afef00db0018c748b6c8035
Ronierison <rmoreira@descartes.com>
2025-03-25 23:02:02 -0300

    add test user creation

--- src/test/java/automation/api/AppTest.java
+++ src/test/java/automation/api/AppTest.java
@@ -1,5 +1,6 @@
 package automation.api;
 
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +10,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
+
     @Test
     public void testUserListData() {
         when().
@@ -17,6 +19,18 @@ public class AppTest {
                 statusCode(HttpStatus.SC_OK).
                 body("page", is(2)).
                 body("data", is(notNullValue()));
+    }
+
+    @Test
+    public void testUserCreationWithSuccess() {
+        given().log().all().
+            contentType(ContentType.JSON).
+            body("{\"name\": \"Roni\", \"job\": \"QA\"}").
+        when().
+            post("https://reqres.in/api/users").
+        then().
+            statusCode(HttpStatus.SC_CREATED).
+            body("name", is("Roni"));
     }
 }
 
